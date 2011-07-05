8d65f0e4b52211aef8f078d475a0eff9e60ab29b
Fellipe Caetano <fellipe.caetano4@gmail.com>
2011-07-05 19:30:58 -0300

    Object blueprints for the creation of controllers and view managers

--- uno.js
+++ uno.js
@@ -1,3 +1,33 @@
+var Controller = function(root) {
+    var self = this;
+    this.root = root;
+    
+    this.getState = function(state) {
+        return root + state;
+    };
+    
+    this.bind = function(state, handler) {
+        $.History.bind(self.getState(state), handler);
+    };
+};
+
+var ViewManager = function(viewContext) {
+    var rootFolder = 'views';
+    var fileSuffix = '.ejs';
+    var contentPlaceholder = '#main';
+    var self = this;
+    self.viewContext = viewContext;
+    
+    self.loadTemplate = function(name, data) {
+        var path = buildViewPath(self.viewContext, name);
+        $(contentPlaceholder).html(path, data);
+    };
+    
+    function buildViewPath(context, name) {
+        return rootFolder + context + '/' + name + fileSuffix;
+    };
+};
+
 $(function() {
     $.History.bind(function(state) {
         var match = state.match(/\?.+$/);
